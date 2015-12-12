4e77cb0619764a6d0343866826994b3193c2b8b3
Shylae <vgalloy@excilys.com>
2015-12-12 11:14:49 +0100

    [ FEAT     ] : Add GoogleService

---
+++ src/main/java/com/vgalloy/server/service/GoogleService.java
@@ -0,0 +1,9 @@
+package com.vgalloy.server.service;
+
+/**
+ * @author Vincent Galloy
+ *         Created by Vincent Galloy on 12/12/15.
+ */
+public interface GoogleService {
+    void updateCalendars();
+}
---
+++ src/main/java/com/vgalloy/server/service/impl/GoogleServiceImpl.java
@@ -0,0 +1,18 @@
+package com.vgalloy.server.service.impl;
+
+import com.vgalloy.server.service.GoogleService;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author Vincent Galloy
+ *         Created by Vincent Galloy on 12/12/15.
+ */
+public class GoogleServiceImpl implements GoogleService {
+    @Override
+    public void updateCalendars() {
+
+    }
+}
