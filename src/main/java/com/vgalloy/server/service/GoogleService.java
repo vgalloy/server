0b276bd472b3485720f7ea91b24d72b88d312859
Shylae <vgalloy@excilys.com>
2015-12-21 20:26:30 +0100

    [ REFACTOR ] : GoogleService

--- src/main/java/com/vgalloy/server/service/GoogleService.java
+++
@@ -1,9 +0,0 @@
-package com.vgalloy.server.service;
-
-/**
- * @author Vincent Galloy
- *         Created by Vincent Galloy on 12/12/15.
- */
-public interface GoogleService {
-    void updateCalendars();
-}
